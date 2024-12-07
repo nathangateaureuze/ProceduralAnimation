cc4b49610de406ffc4625ab35337a0a71c5604f7
nathangateaureuze <92450570+nathangateaureuze@users.noreply.github.com>
2024-12-07 12:40:36 +0100

    Insect - Mesh Members Generation

--- Source/ProceduralAnimation/ProceduralAnimation.Build.cs
+++ Source/ProceduralAnimation/ProceduralAnimation.Build.cs
@@ -8,7 +8,7 @@ public class ProceduralAnimation : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "ControlRig" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
